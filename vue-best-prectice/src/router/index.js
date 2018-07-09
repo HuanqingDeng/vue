5d0165193a6c29d4267ed8b756b4012ba54fe886
HuanqingDeng <1145349052@qq.com>
2018-07-09 21:38:21 +0800

    vue-best-prectice eslint

---
+++ vue-best-prectice/src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import NotFound from '@/components/NotFound'
+import TimeEntries from '@/components/TimeEntries'
+import LogTime from '@/components/LogTime'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '*',
+      component: NotFound
+    },
+    {
+      path: '/time-entries',
+      component: TimeEntries,
+      children: [{
+        path: 'log-time',
+        component: LogTime
+      }]
+    }
+  ]
+})
